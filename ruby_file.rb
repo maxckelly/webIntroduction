6643b33078df12de4e7f88e874e24ef95144d0f5
Max Kelly <max@revelthat.com>
2019-09-24 22:32:09 +1000

    question 7 completed along with question 11

--- ruby_file.rb
+++ ruby_file.rb
@@ -1,6 +1,14 @@
-  celsius = gets.to_i
-  puts celsius
-  fahrenheit = (celsius * 9 / 5) + 32
-  print "The result is: "
-  print fahrenheit
-  puts "."
+  # celsius = gets.to_i
+  # puts celsius
+  # fahrenheit = (celsius * 9 / 5) + 32
+  # print "The result is: "
+  # print fahrenheit
+  # puts "."
+
+arr = [5, 22, 29, 39, 19, 51, 78, 96, 84]
+i = 0
+while (i < arr.size - 1 and arr[i] < arr[i + 1])
+  i = i + 1 end
+puts i
+  arr[i] = arr[i + 1]
+  arr[i + 1] = arr[i]
